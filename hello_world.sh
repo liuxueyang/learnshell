15c92232e141874d53eecbe5cb3c0e0bf24c812d
liuxueyang <zypz457@sina.com>
2014-03-28 22:28:33 +0800

    hello_world

---
+++ hello_world.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: hello_world.sh
+# 
+#         USAGE: ./hello_world.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: liuxueyang (lxy), liuxueyang457@163.com
+#  ORGANIZATION: Hunan University
+#       CREATED: 03/28/2014 21:28
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo -e "Hello World! \a"
+
+exit 0
